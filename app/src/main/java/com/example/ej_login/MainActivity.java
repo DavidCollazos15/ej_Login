ad2485450252b47336023e6b281d06aee04c0e82
DavidGonzálezC <davidgonzalezcgm@gmail.com>
2023-10-05 13:59:39 +0200

    Movil

--- app/src/main/java/com/example/ej_login/MainActivity.java
+++ app/src/main/java/com/example/ej_login/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.ej_login;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
 import android.view.View;
@@ -9,35 +10,40 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText editTextUsername;
-    private EditText editTextPassword;
-    private TextView textViewMessage;
+    private EditText EspacioUsuario;
+    private EditText EspacioContrasena;
+    private TextView MensajeError;
 
 
     //El usuario y contraseña
     private String usuario = "usuario"; // Cambia esto por el usuario correcto
-    private String contrasena = "contrasena"; // Cambia esto por la contraseña correcta
+    private String contrasena = "1234"; // Cambia esto por la contraseña correcta
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        editTextUsername = findViewById(R.id.editTextUsername);
-        editTextPassword = findViewById(R.id.editTextPassword);
-        textViewMessage = findViewById(R.id.textViewMessage);
+        EspacioUsuario = findViewById(R.id.editTextUsername);
+        EspacioContrasena = findViewById(R.id.editTextPassword);
+        MensajeError = findViewById(R.id.textViewMessage);
     }
 
     public void login(View view) {
-        String username = editTextUsername.getText().toString();
-        String password = editTextPassword.getText().toString();
+        String username = EspacioUsuario.getText().toString();
+        String password = EspacioContrasena.getText().toString();
 
         if (username.isEmpty() || password.isEmpty()) {
-            textViewMessage.setText("Faltan datos.");
+            MensajeError.setText("Faltan datos.");
+            MensajeError.setTextColor(ContextCompat.getColor(this, R.color.red)); // Color rojo
+
         } else if (username.equals(usuario) && password.equals(contrasena)) {
-            textViewMessage.setText("Login correcto.");
+            MensajeError.setText("Login correcto.");
+            MensajeError.setTextColor(ContextCompat.getColor(this, R.color.green)); // Color verde
+
         } else {
-            textViewMessage.setText("Login incorrecto.");
+            MensajeError.setText("Login incorrecto.");
+            MensajeError.setTextColor(ContextCompat.getColor(this, R.color.red)); // Color rojo
         }
     }
 }
